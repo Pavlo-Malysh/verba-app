5cf92439ce528db9275894be7758f2c5c5c14bec
Pavlo Malysh <mal.pawlo@gmail.com>
2026-01-25 23:42:20 +0200

    init. create header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,21 @@
+import css from "./Header.module.css";
+
+
+function Header() {
+
+    return (
+        <>
+            <div className={css.headerContainer}>
+                <a href="/" className={css.logoText}> <img width="36" height="44" src="/logo.png" alt="logo" />Verba</a>
+                <nav className={css.navBox}>
+                    <a href="#" className={css.navItem}>Home</a>
+                    <a href="#" className={css.navItem}>Teachers</a>
+                </nav>
+                <button type="button" className={css.loginBtn}><svg width={20} height={20}><use href="/icons.svg#icon-login" className={css.iconLogin}></use></svg>Log in</button>
+                <button type="button" className={css.registerBtn}>Registration</button>
+            </div>
+        </>
+    );
+};
+
+export default Header
