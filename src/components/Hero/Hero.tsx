b2c2e313837bb247e8ae78ccab45fc79277980eb
Pavlo Malysh <mal.pawlo@gmail.com>
2026-03-16 21:33:51 +0200

    replace tag <a> to <Link>. add vercel.json config

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import css from "./Hero.module.css";
 
 function Hero() {
@@ -8,7 +9,7 @@ function Hero() {
                 <div className={css.heroContent}>
                     <h1 className={css.title}>Unlock your potential with the best <span className={css.titleAccent}>language</span> tutors</h1>
                     <p className={css.text}>Embark on an Exciting Language Journey with Expert Language Tutors: Elevate your language proficiency to new heights by connecting with highly qualified and experienced tutors.</p>
-                    <a className={css.link} href="/teachers">Get started</a>
+                    <Link className={css.link} to="/teachers">Get started</Link>
                 </div>
                 <div className={css.heroImgBox}>
                     <img className={css.heroImg} src="/hero.png" alt="" />
